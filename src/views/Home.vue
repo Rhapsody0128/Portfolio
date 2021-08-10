0269d390f714142520842e7f73879ea846e530a2
Rhapsody0128 <61406219+Rhapsody0128@users.noreply.github.com>
2021-08-10 16:26:10 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,6 @@
     section03(:scroll='scroll')
     .footer(:style='footer')
       a.text(href="https://github.com/Rhapsody0128") GitHub
-      a.text(href="https://www.cakeresume.com/s--qzuy_7x1N5rA11p9TaK3rA--/rhapsody0128") CakeResume
       a.text.copyright Copyright© 2020 Rhapsody0128 All rights reserved
 </template>
 
